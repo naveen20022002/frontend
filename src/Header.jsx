ca0e697344fd3f10528965b301ffd3655c08b1b1
Naveen A <121672275+naveen20022002@users.noreply.github.com>
2024-01-29 15:19:09 +0530

    Update Header.jsx tittle

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 -rotate-90">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
             </svg>
-            <span className='font-bold text-xl'>HOTELS</span>
+            <span className='font-bold text-xl'>HOTELSsss</span>
         </Link>
         <div className='flex gap-2 border border-gray-300 rounded-full py-2 px-2 shadow-md shadow-gray-300'>
           <div className='flex items-center '>
